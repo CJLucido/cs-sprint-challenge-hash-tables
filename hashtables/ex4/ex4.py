b0b278659b2bd5170849f83d2aa958e6d5dd7059
Carlo <c.jose.lucido@gmail.com>
2020-07-18 01:59:21 -0700

    ex4 went quick!

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,9 +1,33 @@
-def has_negatives(a):
+def has_negatives(b):
     """
     YOUR CODE HERE
     """
     # Your code here
 
+    analysis_cache = {}
+    result = []
+    for a in b:
+        if a > 0:
+            if analysis_cache.get(a) == "+ Value Only":
+                pass
+            elif analysis_cache.get(a) == "- Value Only":
+                analysis_cache[a] = "Both"
+                result.append(a)
+            else:
+                analysis_cache[a] = "+ Value Only"
+        if a < 0:
+            new_a = a*(-1)
+            print(new_a)
+            if analysis_cache.get(new_a) == "+ Value Only":
+                analysis_cache[new_a] = "Both"
+                result.append(new_a)
+                
+            elif analysis_cache.get(new_a) == "- Value Only":
+                pass
+            else:
+                analysis_cache[new_a] = "- Value Only"
+
+    print(len(analysis_cache))
     return result
 
 
