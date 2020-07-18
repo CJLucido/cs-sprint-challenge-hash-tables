a264241f04296dfca9d56535a826f8f0d0e77bff
Carlo <c.jose.lucido@gmail.com>
2020-07-17 23:26:06 -0700

    ex5 works correctly but it is way to slow so they must want something else

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,12 +1,40 @@
 # Your code here
 
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    all_file_paths =[]
+    new_dict = {}
+    result = []
+
+    for file in files:
+        files_split = file.split("/")
+        all_file_paths.append(files_split)
+        # print(all_file_paths)
+
+    for query in queries:
+        new_dict[query] = []
+        for every_array in all_file_paths:
+            if every_array[-1] == query:
+                new_dict[query].append(every_array)
+            else:
+                pass
+    # print("new_dict", new_dict)
+
+    for query in queries:
+        for path_arr in new_dict[query]:
+            # print("path", path_arr)
+            print("result", result)
+            result.append("/".join(path_arr))
+    
+    for resulting_string in result:
+        if len(resulting_string) < 1:
+            result.remove(resulting_string)
+        else:
+            pass
+    print("result", result)
 
     return result
 
